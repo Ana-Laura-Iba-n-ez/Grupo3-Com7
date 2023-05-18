23483884d7c9928b706cc0d2d19a3a7e654b5ee5
Ana Laura Ibañez <aniibanez20.08@gmail.com>
2023-05-18 19:27:33 -0300

    Función Añadir mejorada

--- func_añadir.py
+++ func_añadir.py
@@ -3,4 +3,13 @@ Función añadir, sólo sirve para el desafío 4: La Inmobiliaria.
 '''
 def añadir():
     print('Inserte los datos del inmueble: ')
-    
+    inmueble = {}
+    inmueble['año'] = input('Año: ')
+    inmueble['metros'] = input('Metros: ')
+    inmueble['habitaciones'] = input('Habitaciones: ')
+    inmueble['garage'] = bool(input('Garage: '))
+    inmueble['zona'] = input('Zona: ')
+    inmueble['estado'] = input('Estado: ')
+    return inmueble
+
+añadir()
