1830e242b66eecb4c8477937832bd9ce9985b46a
Ana Laura Ibañez <aniibanez20.08@gmail.com>
2023-05-18 20:41:45 -0300

    Estructurando 5

--- g3_d4.py
+++ g3_d4.py
@@ -7,15 +7,28 @@ inmuebles = []        #lista con el total de inmuebles
 
 while True:
     print('\nBienvenido/a al sistema de gestión de inmuebles.')
-    venta_o_inventario = input('1 - Realizar una venta.\n'
-                               '2 - Administrar inventario.\n')
-    if venta_o_inventario == '1':
+    eleccion = input('1 - Realizar una venta.\n'
+                     '2 - Administrar inventario.\n'
+                     '3 - Salir del programa.')
+    if eleccion == '1':
          print()
-    elif venta_o_inventario == '2':
+    elif eleccion == '2':
         print('Seleccione una opción: ')
         camino = input('1 - Agregar inmueble.\n2 - Editar inmueble.\n'
                        '3 - Eliminar inmueble.\n')
         if camino == '1':
             inmuebles.append(agregar())
             print(inmuebles)
+        elif camino == '2':
+            print('Ingrese los datos del inmueble que desea editar:')
+        elif camino == '3':
+            print('Ingrese los datos del objeto que desea eliminar:')
+        else:
+            print('No ha seleccionado un número válido, inténtelo de nuevo.')
+            continue
+    elif eleccion == '3':
+        print('Gracias por utilizar Sistema de Gestión de Inmuebles.')
+        break
+    
+
 
