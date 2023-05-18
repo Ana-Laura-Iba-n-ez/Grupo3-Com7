4f0e25056648425d86703bfb82324ae4a4d5e959
Ana Laura Ibañez <aniibanez20.08@gmail.com>
2023-05-18 17:43:08 -0300

    Estructurando 1

--- g3_d4.py
+++ g3_d4.py
@@ -4,3 +4,13 @@ Desafío 4: La inmobiliaria
 inmuebles = []        #tupla con el total de inmuebles
 
 print('Bienvenido/a al sistema de gestión de inmuebles.')
+while True:
+    venta_o_inventario = input('1 - Realizar una venta.\n'
+                               '2 - Administrar inventario.')
+    if venta_o_inventario == 1:
+         print()
+    elif venta_o_inventario == 2:
+        print('Seleccione una opción: ')
+        camino = input('1 - Agregar inmueble.\n2 - Editar inmueble.\n'
+                       '3 - Eliminar inmueble.')
+
