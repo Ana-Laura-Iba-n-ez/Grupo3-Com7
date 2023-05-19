bc65115c1f6bf26a897d8a910b12d5a82b7a8459
Ana Laura Ibañez <aniibanez20.08@gmail.com>
2023-05-19 12:40:42 -0300

    Estructurando 8

--- g3_d4.py
+++ g3_d4.py
@@ -9,8 +9,8 @@ acciones para vender.
 funcion calcular precio.
 Lo que falte de las consignas.
 '''
-import func_agregar
-from func_agregar import agregar
+import funcion_agregar
+from funcion_agregar import agregar
 inmuebles = []        #lista con el total de inmuebles
 
 while True:
@@ -19,18 +19,22 @@ while True:
                      '2 - Administrar inventario.\n'
                      '3 - Salir del programa.\n')
     if eleccion == '1':
-         print()
+        print('Venta:')
+        print('Inserte los datos del inmueble que desea vender: ')
+        buscar = agregar()
+        for i in inmuebles:
+            if i in buscar:
+                
+
     elif eleccion == '2':
         print('Seleccione una opción: ')
         inventario = input('1 - Agregar inmueble.\n2 - Editar inmueble.\n'
                            '3 - Eliminar inmueble.\n')
         if inventario == '1':
             inmuebles.append(agregar())
-            print(inmuebles)
             for i in inmuebles:
                 if i == False:
                     inmuebles.pop()
-                    print(inmuebles)
         elif inventario == '2':
             print('Ingrese los datos del inmueble que desea editar:')
         elif inventario == '3':
