0dcc85d852c94880426eaaad78f445f1ee6f9c02
Ana Laura Ibañez <aniibanez20.08@gmail.com>
2023-05-19 13:29:41 -0300

    Nuevas Funciones

---
+++ Buscar Inmueble por presupuesto.py
@@ -0,0 +1,8 @@
+def
+presupuesto = int(input('Ingrese el monto del presupuesto '))
+en_presupuesto:
+for inmueble in inmuebles:
+    if inmueble [ 'precio' ] <= presupuesto
+and inmueble [ 'estado' ] in [ 'Disponible', 'Reservado' ]:
+    en_presupuesto.append(inmueble)
+  return en_presupuesto
