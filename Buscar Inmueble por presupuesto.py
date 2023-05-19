d732abc99f0ec65dfaf8d88dbecadac66c1c9f4d
Ana Laura Ibañez <aniibanez20.08@gmail.com>
2023-05-19 13:51:34 -0300

    Revisión de funciones buscar por presup y calcular presup

--- Buscar Inmueble por presupuesto.py
+++ Buscar Inmueble por presupuesto.py
@@ -1,8 +1,11 @@
-def
-presupuesto = int(input('Ingrese el monto del presupuesto '))
-en_presupuesto:
-for inmueble in inmuebles:
-    if inmueble [ 'precio' ] <= presupuesto
-and inmueble [ 'estado' ] in [ 'Disponible', 'Reservado' ]:
-    en_presupuesto.append(inmueble)
-  return en_presupuesto
+import funcion_calcular_precio
+from funcion_calcular_precio import calcular_precio
+
+def buscar_presupuesto(inmuebles):
+    presupuesto = int(input('Ingrese el monto del presupuesto: '))
+    en_presupuesto = []
+    for inmueble in inmuebles:
+        if (calcular_precio(inmueble) <= presupuesto and 
+            inmueble ['estado'] in ['Disponible', 'Reservado']):
+         en_presupuesto.append(inmueble)
+    return en_presupuesto
--- funcion_calcular_precio.py
+++ funcion_calcular_precio.py
@@ -1,7 +1,7 @@
 '''
 Función para calcular el precio de un inmueble en función de la zona
 '''
-def calcularPrecio(inmueble):
+def calcular_precio(inmueble):
     precio_base = inmueble["metros"] * 100 + inmueble["habitaciones"] * 500 + int(inmueble["garaje"]) * 1500
     antiguedad = 2023 - inmueble["año"]
     if inmueble["zona"] == "A":
